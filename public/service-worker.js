f700fcff76e59d1db008ae67f66d269f5b4096c8
Shambhavi7gupta <shambhavi05gupta@gmail.com>
2024-06-29 16:26:49 +0530

    Changes

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,38 @@
-self.addEventListener("install", (event) => {
-  console.log("Service worker installing...");
-  self.skipWaiting();
+// Service Worker Code
+
+const CACHE_NAME = "my-pwa-cache-v1";
+const urlsToCache = ["index.html", "offline.html"];
+
+this.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened Cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener("activate", (event) => {
-  console.log("Service worker activating...");
+this.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
 
-self.addEventListener("fetch", (event) => {
-  console.log("Fetching:", event.request.url);
+this.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
 });
